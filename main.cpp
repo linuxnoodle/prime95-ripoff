f5d3da457d3bc9e951817c391d4d4a2145f2825a
linuxnoodle <ipadnoodle@gmail.com>
2021-03-17 22:04:47 -0700

    Added windows support

--- main.cpp
+++ main.cpp
@@ -1,33 +1,86 @@
-#include <thread>
-#include <random>
+#ifdef __unix__
+  #include <thread>
+#elif defined(_WIN32) || defined(__WIN32)
+  #include <windows.h>
+#endif
+#include <climits>
 #include <iostream>
+#include <random>
+#include <chrono>
 
-// Checks if a random number is prime.
-// Change this to any different algorithm if you want.
-void threadStresser(){
-    int randomNumber = rand();
-    int factorCount = 0;
-    for (int i = randomNumber - 1; i > 1; --i){
-        if (randomNumber % i == 0){
-            ++factorCount;
+std::mt19937 generator(time(0));
+std::uniform_int_distribution<std::mt19937::result_type> distribution(0, INT_MAX);
+
+#ifdef __unix__
+    // Checks if a random number is prime.
+    // Change this to any different algorithm if you want.
+    void threadStresser(){
+        int randomNumber = distribution(generator);
+        int factorCount = 0;
+        for (int i = randomNumber - 1; i > 1; --i){
+            if (randomNumber % i == 0){
+                ++factorCount;
+            }
         }
+        std::cout << randomNumber << ": factor count? " << factorCount << "\n";
     }
-    std::cout << randomNumber << ": factor count? " << factorCount << "\n";
-}
+#elif defined(_WIN32) || defined(__WIN32)
+    // Checks if a random number is prime, but using windows.
+    // Change this to any different algorithm if you want.
+    DWORD WINAPI threadStresser(LPVOID lpParameter){
+        int randomNumber = distribution(generator);
+        int factorCount = 0;
+        for (int i = randomNumber - 1; i > 1; --i){
+            if (randomNumber % i == 0){
+                ++factorCount;
+            }
+        }
+        std::cout << randomNumber << ": factor count? " << factorCount << "\n";
+    }
+#endif
 
 int main(){
-    // Get thread count.
-    unsigned int threadCount = std::thread::hardware_concurrency();
-    if (!threadCount)
-        ++threadCount;
-    std::thread threads[threadCount];
+    #ifdef __unix__
+        // Get thread count.
+        unsigned int threadCount = std::thread::hardware_concurrency();
+        if (!threadCount)
+            ++threadCount;
+        std::thread threads[threadCount];
 
-    while (true){
-        for (int i = 0; i < threadCount; ++i){
-            threads[i] = std::thread(threadStresser);
-            threads[i].detach();
+        while (true){
+            for (int i = 0; i < threadCount; ++i){
+                threads[i] = std::thread(threadStresser);
+                threads[i].detach();
+            }
         }
-    }
-
+    #elif defined(_WIN32) || defined(__WIN32)
+        // Get thread count.
+        SYSTEM_INFO sysinfo;
+        GetSystemInfo(&sysinfo);
+        unsigned int threadCount = sysinfo.dwNumberOfProcessors;
+        
+        HANDLE threads[threadCount];
+        while (true){
+            // Yes, I know that it will only close threads when all of then are done.
+            // No, I probably won't fix this.
+            // Porting this over to windows was already way too much effort.
+            for (int i = 0; i < threadCount; ++i){
+                threads[i] = CreateThread(
+                    NULL,
+                    0,
+                    threadStresser,
+                    NULL,
+                    0,
+                    NULL);
+                if (threads[i] == NULL){
+                    return -1;
+                }
+            }
+            for (int i = 0; i < threadCount; ++i){
+                WaitForSingleObject(threads[i], INFINITE);
+                CloseHandle(threads[i]);
+            }
+        }
+    #endif
     return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,9 @@
 CC=g++
-CPPFLAGS=-O0 -std=c++11 -pthread
+ifeq ($(OS), Windows_NT)
+	CPPFLAGS=-O0 -std=c++11
+else
+	CPPFLAGS=-O0 -std=c++11 -pthread
+endif
 
 smasher: main.cpp
 	$(CC) $(CPPFLAGS) main.cpp -o smasher
